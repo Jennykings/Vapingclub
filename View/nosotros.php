9e796fe846062d800719a71c302179fc395b7e5d
DanniloTorres <125678348+DanniloTorres@users.noreply.github.com>
2023-04-10 11:52:58 -0500

    pagina-Nosotros-banner
    responsive

--- View/nosotros.php
+++ View/nosotros.php
@@ -18,7 +18,107 @@
 
 <body>
     <?php include 'header.html' ?>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+    <br><br>
+    <section id="banner">
+
+
+        <div class="contenido-banner">
+            <h3> <span>Vaping Cloud</span> <br><br> <br> Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br>
+                Non adipisci incidunt et mollitia voluptatibus maxime assumenda architecto dolorum vitae quis,<br> iusto
+                culpa ipsum autem doloremque commodi quo quasi suscipit rem iusto culpa ipsum autem doloremque<br>
+                commodi quo quasi suscipit rem commodi quo quasi suscipit rem </h3>
+
+        </div>
+    </section>
+
+</body>
+
+</html>
+
+
+
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+
+    #banner {
+        padding: 0 50px;
+        background-image: url(../img/nosotros.jpeg);
+        height: 80vh;
+        background-size: cover;
+        background-position: center;
+    }
+
+    #banner::before {
+        content: '';
+        position: absolute;
+        width: 100%;
+        left: 0;
+    }
+
+    .contenido-banner {
+        position: relative;
+        color: #fff;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+
+    .contenido-banner h3 {
+        font-size: 20px;
+        font-weight: 400;
+        padding: 10px 0px;
+        line-height: 1.2;
+
+    }
+
+    .contenido-banner h3 span {
+        font-weight: 600;
+        font-size: 90px;
+    }
+
+    @media screen and (max-width: 768px) {
+        #banner {
+            height: 50vh;
+        }
+
+        .contenido-banner h3 {
+            font-size: 15px;
+        }
+
+        .contenido-banner h3 span {
+        font-weight: 600;
+        font-size: 50px;
+    }
+
+    }
+
+    @media screen and (max-width: 347px) {
+        #banner {
+            height: 50vh;
+        }
+
+        .contenido-banner h3 {
+            font-size: 10px;
+        }
+
+        .contenido-banner h3 span {
+        font-weight: 600;
+        font-size: 40px;
+    }
+
+    }
+</style>
+
     <?php include 'footer.html' ?>
 </body>
 
